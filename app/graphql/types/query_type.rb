c3717088b945a4648e2d4f93f417283ed41466f0
Thomas Grinstead <tomahawkga@gmail.com>
2020-10-17 12:24:09 -0400

    create group to user relation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,15 +3,20 @@ module Types
     field :event, Types::EventType, null: false do
       argument :id, ID, required: true
     end
-
-    field :events, [Types::EventType], null: false
-
     def event(id:)
       Event.find(id)
     end
 
+    field :events, [Types::EventType], null: false
     def events
       Event.all
+    end
+
+    field :group, Types::GroupType, null: false do
+      argument, :id, ID, required: true
+    end
+    def group(id:)
+      Group.find(id)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_secure_password :recovery_password, validations: false
+
+  has_and_belongs_to_many :group
 end
